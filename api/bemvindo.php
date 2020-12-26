41baf2902095ee5f41554569233f52dffeb42b45
eFrostByte <59237723+eFrostByte@users.noreply.github.com>
2020-12-26 14:55:27 -0300

    ./

--- api/bemvindo.php
+++ api/bemvindo.php
@@ -3,14 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="stuff/fstyle.css">
+<link rel="stylesheet" href="./stuff/fstyle.css">
   <title>Seja bem vindo!</title>
 </head>
 <body>
-  <audio preload="auto" src="stuff/mus1.mp3" autoplay loop></audio>
+  <audio preload="auto" src="./stuff/mus1.mp3" autoplay loop></audio>
   <script> document.getElementsByTagName('audio')[0].volume = 0.25;</script>
   <div class="center" >
-    <img src="tela1.jpg" alt="Faiou!" class="image" usemap="#no">
+    <img src="./tela1.jpg" alt="Faiou!" class="image" usemap="#no">
   <map name="no">
    <area class="area" shape="poly" coords="312,122,385,96, 385, 400, 314, 400" href="falso/aqui.html" alt="Faiou!"/>
   </map>
