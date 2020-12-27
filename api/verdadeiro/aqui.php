9b77ea41246ec6795f6eb4d8fc17b75e39d6b28a
eFrostByte <59237723+eFrostByte@users.noreply.github.com>
2020-12-27 19:22:04 -0300

    listener and img

--- api/verdadeiro/aqui.php
+++ api/verdadeiro/aqui.php
@@ -11,19 +11,10 @@
 	<div class="level" id="level-number">
 		<h1>Nível 3</h1>
 	</div>
-	<script type="text/javascript">
-		const level_div = document.getElementById('level-number');
-		const audio = document.getElementsByTagName('audio')[0]
-
-		document.addEventListener('click', () => {
-			level_div.style = 'display: none;';
-			audio.play();
-		})
-	</script>
-    <!--<audio preload="auto" src="../stuff/mus1.mp3" autoplay loop></audio>-->
+	<script src="/stuff/listener.js" type="text/javascript"></script>
     <script> document.getElementsByTagName('audio')[0].volume = 0.25;</script>
     <div class="center">
-    	<img src="/images/tela3.jpg" alt="Vish..." class="grand3"/>
+    	<img src="/images/verdadeiro/tela3.jpg" alt="Vish..." class="grand3"/>
     	<img src="/stuff/google.png" alt="Google..." class="google">
     	<p class="p-dica">Tenho coração mas não tenho bigode</p>
     	<div align="center" class="bw">
