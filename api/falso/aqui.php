44176c607681058e8d248143e6a89fb868caf5fe
eFrostByte <59237723+eFrostByte@users.noreply.github.com>
2020-12-27 13:56:52 -0300

    falso

---
+++ api/falso/aqui.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/stuff/fstyle.css"  />
+  <title>Como os antigos...</title>
+</head>
+<body>
+  <audio preload="auto" src="/stuff/mus1.mp3" autoplay loop></audio>
+  <script> document.getElementsByTagName('audio')[0].volume = 0.25;</script>
+ <div class="center">
+  <img src="/images/door/tela2.jpg" alt="Vish..."/>
+  
+  </div>
+  <!-- Pare de ser tão negativo!-->
+</body>
+</html>
