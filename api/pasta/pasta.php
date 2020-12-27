7752fa7a2107884057b85bf8d2612f73bcd62752
eFrostByte <59237723+eFrostByte@users.noreply.github.com>
2020-12-26 21:47:37 -0300

    Fstyle

--- api/pasta/pasta.php
+++ api/pasta/pasta.php
@@ -1,8 +1,10 @@
 <html>
   <head>
     <title>PHP Test</title>
+    <link rel="stylesheet" href="../stuf/fstyle.css">
   </head>
   <body>
+    <button> Clique </button>
     <?php echo '<p>Hello Pasta!!!!</p>'; ?> 
   </body>
 </html>
