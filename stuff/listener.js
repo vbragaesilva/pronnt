404e1748a83afc65fff85a98fbb807aacbe93791
eFrostByte <59237723+eFrostByte@users.noreply.github.com>
2020-12-28 15:45:52 -0300

    Update listener.js

--- stuff/listener.js
+++ stuff/listener.js
@@ -1,7 +1,7 @@
-const level_div = document.getElementById('level-number');
-		const audio = document.getElementsByTagName('audio')[0]
+const level_div = document.getElementsByClassName('level')[0];
+const audio = document.getElementsByTagName('audio')[0]
 
-		document.addEventListener('click', () => {
-			level_div.style = 'display: none;';
-			audio.play();
-		})
+document.addEventListener('click', () => {
+	level_div.style = 'display: none;';
+	audio.play();
+})
