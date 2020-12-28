f9bead7342d730d047459db4bae1ca2109af2208
eFrostByte <59237723+eFrostByte@users.noreply.github.com>
2020-12-28 15:21:37 -0300

    trancado

--- api/level2/trancado.php
+++ api/level2/trancado.php
@@ -3,17 +3,17 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="../stuff/fstyle.css">
+	<link rel="stylesheet" type="text/css" href="/stuff/fstyle.css">
 	<title>Sem a chave</title>
 	<script type="text/javascript">function al(){alert('TRANCADO!')}</script>
 </head>
 <body>
-	<audio preload="auto" src="../stuff/mus1.mp3" autoplay loop></audio>
+	<audio preload="auto" src="/stuff/mus1.mp3" autoplay loop></audio>
   	<script> document.getElementsByTagName('audio')[0].volume = 0.25;</script>
  	<div class="center">
- 		<img src="../../images/door/tela2.jpg" alt="Vish..." usemap="#no" />
+ 		<img src="/images/door/tela2.jpg" alt="Vish..." usemap="#no" />
  		<map name="no">
- 			<area shape="circle" coords="255, 240, 25" href="" onmouseover="al()">
+ 			<area shape="circle" coords="255, 240, 25" href="#" onmouseover="al()">
  		</map>
   		<p class="p-dica">Seja esperto</p>
  	</div>
