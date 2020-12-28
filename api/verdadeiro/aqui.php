22e61543c6702d41d307c18013bfbbdf3dd63b87
eFrostByte <59237723+eFrostByte@users.noreply.github.com>
2020-12-27 23:24:54 -0300

    name -> map // name -X-> /map X_X

--- api/verdadeiro/aqui.php
+++ api/verdadeiro/aqui.php
@@ -15,9 +15,9 @@
     <script> document.getElementsByTagName('audio')[0].volume = 0.25;</script>
     <div class="center">
     	<img src="/images/verdadeiro/tela3.jpg" alt="Vish..." class="grand3" usemap="#no"/>
-	<map>
+	<map name="no">
 		<area class="area" shape="poly" coords="85, 200, 156, 110, 148, 68, 97, 68, 85, 95, 74, 68, 20, 68, 14, 110" href="level4/jose.php" alt="Faiou!"/>
-	</map name="no">
+	</map>
     	<img src="/stuff/google.png" alt="Google..." class="google">
     	<p class="p-dica">Tenho coração mas não tenho bigode</p>
     </div>
