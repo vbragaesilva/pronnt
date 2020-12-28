e01cc2efe4c22e64ea855ec0ce732d9ac7096aac
eFrostByte <59237723+eFrostByte@users.noreply.github.com>
2020-12-27 23:03:12 -0300

    heart map

--- api/verdadeiro/aqui.php
+++ api/verdadeiro/aqui.php
@@ -14,12 +14,12 @@
 	<script src="/stuff/listener.js" type="text/javascript"></script>
     <script> document.getElementsByTagName('audio')[0].volume = 0.25;</script>
     <div class="center">
-    	<img src="/images/verdadeiro/tela3.jpg" alt="Vish..." class="grand3"/>
+    	<img src="/images/verdadeiro/tela3.jpg" alt="Vish..." class="grand3" usemap="#no"/>
+	<map>
+		<area class="area" shape="poly" coords="129, 300, 240, 165, 226, 103, 147, 101, 130, 145, 112, 102, 30, 103, 20, 165" href="level4/jose.php" alt="Faiou!"/>
+	</map>
     	<img src="/stuff/google.png" alt="Google..." class="google">
     	<p class="p-dica">Tenho coração mas não tenho bigode</p>
-    	<div align="center" class="bw">
-      		<button onclick="check()">Responder</button>
-    	</div>
     </div>
 
 <!-- Vida longa ao rei!! -->
